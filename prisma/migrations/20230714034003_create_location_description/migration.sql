9773bad8e6d1c976d59a7aaca28c3a4bed832d47
Diogo Marques <diogo.ageflex@hotmail.com>
2023-07-14 01:06:01 -0300

    feat: add location description

---
+++ prisma/migrations/20230714034003_create_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ALTER COLUMN "locationDescription" SET DEFAULT 'Morbi a metus. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Nunc tincidunt ante vitae massa. Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Fusce fermentum.';
