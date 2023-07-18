fa96db32244ad3395a6f76d0ff0af1c7884b6031
Diogo Marques <diogo.ageflex@hotmail.com>
2023-07-18 00:17:34 -0300

    feat: add require validation to trip reservation

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -5,28 +5,96 @@ import React from 'react'
 import Input from '@/components/Input'
 import { Trip } from '@prisma/client'
 import Button from '@/components/Button'
+import { useForm, Controller } from 'react-hook-form'
 
 interface TripReservationProps {
     trip: Trip
 }
 
+interface TripReservationForm {
+    guests: number;
+    startDate?: Date | null;
+    endDate?: Date | null;
+}
+
 const TripReservation = ({trip}: TripReservationProps) => {
+    const {
+        register, 
+        handleSubmit, 
+        formState: { errors },
+        control,
+    } = useForm<TripReservationForm>();
+
+    const onSubmit = (data: any) => {
+        console.log(data);
+    }
+
     return (
         <div>
             <div className="flex flex-col px-5 pb-10">
                 <div className="flex gap-4">
-                    <DatePicker placeholderText="Data de ida" onChange={() => {}} className="w-full"/>
-                    <DatePicker onChange={() => {}} className="w-full"/>
+                    <Controller 
+                        name="startDate"
+                        rules={{
+                            required: {
+                                value: true,
+                                message: "Data de ida é obrigatória"
+                            }
+                        }}
+                        control={control}
+                        render={({ field }) => (
+                        <DatePicker 
+                            error={!!errors?.startDate} 
+                            errorMessage={errors?.startDate?.message} 
+                            onChange={field.onChange} 
+                            selected={field.value} 
+                            placeholderText="Data de ida" 
+                            className="w-full"
+                        />
+                        )}
+                    />
+                    <Controller 
+                        name="endDate"
+                        rules={{
+                            required: {
+                                value: true,
+                                message: "Data de volta é obrigatória"
+                            }
+                        }}
+                        control={control}
+                        render={({ field }) => (
+                        <DatePicker 
+                            error={!!errors?.endDate} 
+                            errorMessage={errors?.endDate?.message} 
+                            onChange={field.onChange} 
+                            selected={field.value} 
+                            placeholderText="Data final" 
+                            className="w-full"
+                        />
+                        )}
+                    />
                 </div>
 
-                <Input placeholder={`Número de hóspedes (max: ${trip.maxGuests})`} className="mt-4" />
+                <Input
+                {...register("guests",{
+                    required:{
+                        value: true,
+                        message: "Numero de hóspedes é obrigatório"
+                    }
+
+                })} 
+                placeholder={`Número de hóspedes (max: ${trip.maxGuests})`} 
+                className="mt-4"
+                error={!!errors?.guests}
+                errorMessage={errors?.guests?.message}
+                />
 
                 <div className="flex justify-between mt-4">
                     <p className="font-medium text-sm text-secondary">Total: </p>
                     <p className="font-medium text-sm text-secondary">R$ 2.500 </p>
                 </div>
                 <div className="border-b border-grayLighter">
-                    <Button variant="primary"className="mt-3 w-full">Reservar Agora</Button>
+                    <Button onClick={() => handleSubmit(onSubmit)()} variant="primary"className="mt-3 w-full">Reservar Agora</Button>
                 </div>
                 
             </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Footer = () => {
     return (
         <div className="p-4 flex flex-col justify-center items-center">
-            <Image src="/logo.png" width={200} height={200} alt="Logo" />
+            <Image src="/logo-born.png" width={120} height={80} alt="Logo" className="pb-2" />
             <p className="text-sm text-secondary">Todos os direitos reservados</p>
 
         </div>
